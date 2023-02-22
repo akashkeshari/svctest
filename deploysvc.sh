7fa7361e21cf1c7cde08bcd7c11732c2a3ebcce1
Akash Keshari <akkeshar@microsoft.com>
2023-02-22 11:53:19 +0530

    updated deploy.sh

--- deploysvc.sh
+++ deploysvc.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-for i in {20001..30000}
+for i in {1..100000}
 do
    echo "Iteration $i"
    export INSTANCE=$i
    envsubst < svc.yaml | kubectl apply -f -
-done
-
-for i in {20001..30000}
-do
-   echo "Iteration $i"
-   export INSTANCE=$i
+   sleep 5
    envsubst < svc.yaml | kubectl delete -f -
 done
